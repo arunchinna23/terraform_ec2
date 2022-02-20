de8682a1fc35ac55b382690d552f88c518eab616
arunchinna23 <93465450+arunchinna23@users.noreply.github.com>
2022-02-20 13:51:24 +0530

    Create vpc4.tf

---
+++ vpc4/vpc4.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = "us-east-1"
+  access_key = "AKIAVZV6K3H77CF2763W"
+  secret_key = "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"
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/24"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "vpc2"
+  }
+}
