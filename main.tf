723d93453896cdd5fb588e1058f82e738f02809a
arunchinna23 <93465450+arunchinna23@users.noreply.github.com>
2022-02-20 12:10:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_eip" "elasticip" {
   vpc      = true
 }
 resource "aws_instance" "ubuntu" {
-  ami           = "ami-04505e74c0741db8d"
+  ami           = "ami-033b95fb8079dc481" 
   instance_type = "t2.micro"
   key_name = "NVirginia"
   subnet_id = aws_subnet.pub.id
